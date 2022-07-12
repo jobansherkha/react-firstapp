2cb2aa7bb63dcb3b3bda7c5f4f6449e8c423f5b4
jobansherkha <jobansherkha@gmail.com>
2022-07-12 16:56:47 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1> Hello world !!!!!11</h1>
-    </div>
+
+  function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+  }
+  function getGreeting(user) {
+    if (user) {
+      return <h1>Hello, {formatName(user)}!</h1>;
+    }
+    return <h1>Hello, Stranger.</h1>;
+  }
+  const user = {
+    firstName: 'joban',
+    lastName: 'sherkha'
+  };
+
+  const element = (
+    <h1>
+      Hello, {formatName(user)}!
+    </h1>
   );
+  return (
+    getGreeting(user))
 }
 
 export default App;
